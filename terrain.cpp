7c495f6a8d207adb1f1b45b62b66b74ec09457b8
Kiran Prasad <kiran.prasad@hotmail.com>
2017-04-18 02:47:15 +0530

    Terrain can now be traversed without actually following the contours. TODO: Terrain traversal following contours. Skybox integration.

--- terrain.cpp
+++ terrain.cpp
@@ -8,25 +8,27 @@
 
 using namespace std;
 
-GLdouble viewpos = 550.0;
+GLdouble viewposx = 400.0;
+GLdouble viewposz = 400.0;
+GLdouble terrainviewer[] = {400.0,50.0,400.0};
 int x, z, xmin = 0, zmin = 0, xmax = 400, zmax = 400;
 GLfloat map[500][500]={0};	//map[][] stores the height values for each intersection point on the grid.
 int iter = 0;
-
+float theta[]={5.0,0.0,5.0};
 
 void terrainCalc(){	
 	GLfloat dely;
 	float v, a, b, c, d;
 
-	while(iter <100){
+	while(iter <256){
 
-		(iter>50)?dely=0.5:dely=1; //Amazingly dynamic displacement formula 10/10
-
+		(iter>128)?dely+=0.5/iter:dely=0.5; //Amazingly dynamic displacement formula 10/10. Don't ask don't tell shh. I should stop writing edgy comments.
+		srand(iter);
 		v= rand();
 		a= sin(v);
 		b= cos(v);
 		d= sqrt(pow((xmax-xmin),2)+pow((zmax-zmin),2));
-		c= (rand()/RAND_MAX)* d - d/2;
+		c= (rand()/RAND_MAX)* d - d/2.4;
 
 		for(x=xmin;x<=xmax;x+=5) //This loop caculates the y plane values for each point on the grid.
 			for(z=zmin;z<=zmax;z+=5)
@@ -41,67 +43,93 @@ void terrainCalc(){
 
 
 void terrainGen(){
+	glutFullScreen();
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	gluLookAt(terrainviewer[0],terrainviewer[1],terrainviewer[2],200,0,200, 0,100,0);
+	glRotatef(theta[0],1.0,0.0,0.0);
+	glRotatef(theta[1],0.0,1.0,0.0);
+	glRotatef(theta[2],0.0,0.0,1.0);	
 
-    terrainCalc();    
-	
+
 	int x, z;	
 
 	for(x=xmin;x<=xmax;x+=5)	//Loop to draw the primitives
 		for(z=zmin;z<=zmax;z+=5){
 			glBegin(GL_POLYGON);
+			glColor3f(0.7,0.7,0.7);
 			glVertex3f(x,map[x][z],z);
-			glColor3f(0.0,0.0,0.0);
+			glColor3f(0.3,0.3,0.3);
 			glVertex3f(x+5,map[x+5][z],z);
-			glColor3f(1.0,0.0,0.0);
+			glColor3f(0.3,0.3,0.3);
 			glVertex3f(x+5,map[x+5][z+5],z+5);
-			glColor3f(0.0,0.0,0.0);
+			glColor3f(0.7,0.7,0.7);
 			glVertex3f(x,map[x][z+5],z+5);
 			glEnd();
 		}
 	glFlush();
+
+glutSwapBuffers();
 
 }
 
 
 //F.S. Hill textbook
 
-void init(int width, int height){
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0,1.0,1.0,1.0);
-	glColor3f(1.0,0.0,0.0);
+void size(int width,int height){
+
+	glViewport(0,0,width, height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(50.0,GLdouble(width)/GLdouble(height),100.0, 600.0);				//void gluPerspective(GLdouble fovy, GLdouble aspect, GLdouble zNear, GLdouble zFar);
+	glFrustum(-2.0,2.0,-2*(GLfloat)width/(GLfloat)height,2*(GLfloat)width/(GLfloat)height, 2.5,20.0);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(viewpos,150.0,viewpos, 0.0,0.0,0.0, 0.0,100.0,0.0);	//void gluLookAt(GLdouble eyeX,	GLdouble eyeY, GLdouble eyeZ, GLdouble centerX, GLdouble centerY, GLdouble centerZ, GLdouble upX, GLdouble upY, GLdouble upZ);
-	glEnable(GL_DEPTH_TEST);	
+
 }
 
-void inout(int key, int x, int y){
 
-	if(key == GLUT_KEY_UP){
-		viewpos -= 10.0;
-		glutReshapeFunc(init);
-	}
-	else if(key == GLUT_KEY_DOWN){
-		viewpos += 10.0;
-		glutReshapeFunc(init);
-	}
+void keymove(int key, int x, int y){
+	if(key==GLUT_KEY_UP) 
+		{
+		if(terrainviewer[0]<=400.0 && terrainviewer[2]<=400.0){ 
+		terrainviewer[0]-=1.0;
+		terrainviewer[2]-=1.0;
+			}
+		}
+	else if(key==GLUT_KEY_DOWN)
+		{if(terrainviewer[0]>=0.0 && terrainviewer[2]>=0.0 && terrainviewer[0]<400.0 && terrainviewer[2]<400.0){
+		terrainviewer[0]+=1.0;
+		terrainviewer[2]+=1.0;}}
+/*	else if(key==GLUT_KEY_LEFT) {turn(GLUT_KEY_LEFT); terrainGen();}
+	else if(key==GLUT_KEY_RIGHT){turn(GLUT_KEY_RIGHT); terrainGen();}*/
+glutPostRedisplay();
 }
 
+
+/*void mouse(int but, int state, int x, int y){
+if(but==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
+int xworld = sin(theta + dtheta);
+int zworld = -cos(theta+dtheta);
+glTranslatef(xworld,0,zworld);
+terrainGen();
+}
+
+}*/
 
 int main(int argc, char** argv){
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE|GLUT_DEPTH);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA|GLUT_DEPTH);
 	glutInitWindowPosition(0,0);
 	glutInitWindowSize(500,500);
 	glutCreateWindow("Terrain");
+	terrainCalc();
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-	glClearColor(1.0,1.0,1.0,1.0);
-	glutReshapeFunc(init);
+	glClearColor(0.0,0.0,0.0,1.0);
+	glutReshapeFunc(size);
 	glutDisplayFunc(terrainGen);
-	glutSpecialFunc(inout);
+	glutIdleFunc(terrainGen);
+	glutSpecialFunc(keymove);
+//	glutMouseFunc(mouse);
+	glEnable(GL_DEPTH_TEST);	
 	glutMainLoop();
 	return 0;
 
