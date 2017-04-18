a97f1496ad928d3d8707601abd744802900ee853
Mayur Bhatolia <mayur.bhatolia@gmail.com>
2017-04-18 22:27:52 +0530

    Skybox with initial grid terrain.

---
+++ msb.cpp
@@ -0,0 +1,276 @@
+#include<stdlib.h>
+#include<GL/glut.h>
+#include<stdio.h>
+#include<math.h>
+#include "terrain.h"
+
+
+void display();
+
+const int SKY_FRONT=0,SKY_RIGHT=1,SKY_LEFT=2,SKY_BACK=3,SKY_UP=4,SKY_DOWN=5; //for skybox matrix
+int prevx=0;
+GLint skybox[6],moon,x_r=0, y_r=0, z_r=0;
+GLfloat viewer[3] = {1.0f, 0.0f, 0.0f},camera[3] = {0.0f, 0.0, 0.0};
+GLdouble curr=0, prev=0,movcord[3]={-200,-10,-200};
+
+
+
+
+GLuint LoadBMP(const char *fileName)
+{
+	FILE *file;
+	unsigned char header[54],*data;
+	unsigned int dataPos,size,width, height;
+	file = fopen(fileName, "rb");
+	fread(header, 1, 54, file);				//Windows BMP begin with 54 byte header
+	dataPos		= *(int*)&(header[0x0A]);	//dec10, Actual BMP data
+	size		= *(int*)&(header[0x22]);	//dec34, BMP Size
+	width		= *(int*)&(header[0x12]);	//dec18, Image Width
+	height		= *(int*)&(header[0x16]);	//dec22, Image Height
+	if (size == NULL)
+		size = width * height * 3;
+	if (dataPos == NULL)
+		dataPos = 54;
+	data = new unsigned char[size];
+	fread(data, 1, size, file);
+	fclose(file);
+	GLuint texture;
+	glGenTextures(1, &texture);				//Generate (allocate) 1 texture name
+	glBindTexture(GL_TEXTURE_2D, texture);	//Bind the 2D texture
+
+
+
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);	//MAG filter
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);	//MIN filter
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, data); //target, level, internalFormat, width, height,border, format, type, data 
+	return texture;
+}
+
+void initSky()
+{
+	glShadeModel(GL_SMOOTH);							
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);				
+	glClearDepth(1.0f);									
+	glEnable(GL_DEPTH_TEST);														
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	
+	//load BMP images into the skybox matrix
+		skybox[SKY_DOWN] = LoadBMP("BMP/down.bmp");
+		skybox[SKY_FRONT] = LoadBMP("BMP/front.bmp");
+		skybox[SKY_BACK] = LoadBMP("BMP/back.bmp");
+		skybox[SKY_RIGHT] = LoadBMP("BMP/right.bmp");
+		skybox[SKY_LEFT] = LoadBMP("BMP/left.bmp");
+		skybox[SKY_UP] = LoadBMP("BMP/up.bmp");
+		moon=LoadBMP("BMP/moon.bmp");
+}
+
+void initLights()
+{
+
+	GLfloat whiteSpecularMaterial[] = {5.0,5.0,5.0},light_post0[]={0.0,0.0,10.0,1.0},whiteSpecularLight[] = {5.0, 5.0, 5.0},blackAmbientLight[] = {0.3, 0.3, 0.3},whiteDiffuseLight[] = {1.0, 1.0, 1.0},mShininess[] = {50},twoModel[]={GL_TRUE}; 
+	glEnable (GL_DEPTH_TEST);
+	glEnable (GL_LIGHTING);
+    glEnable (GL_LIGHT0);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, whiteSpecularLight);
+   	glLightfv(GL_LIGHT0, GL_AMBIENT, blackAmbientLight);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, whiteDiffuseLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_post0);
+	glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, twoModel);
+ 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, whiteSpecularMaterial);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mShininess);
+}
+
+void Draw_Skybox(float x, float y, float z, float width, float height, float length){
+
+	glDisable(GL_DEPTH_TEST);
+	x = x - width  / 2;
+	y = y - height / 2;
+	z = z - length / 2;
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D,skybox[SKY_UP]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x+width, y+height, z);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x+width, y+height, z+length);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x,		  y+height,	z+length);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+height,	z); 
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D,skybox[SKY_FRONT]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x+width, y,		z);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x+width, y,		z+length);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x+width, y+height,	z+length); 
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x+width, y+height,	z);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D,skybox[SKY_BACK]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x,		  y+height,	z);	
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+height,	z+length); 
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x,		  y,		z+length);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x,		  y,		z);		
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D,skybox[SKY_RIGHT]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x+width, y,		z);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x+width, y+height, z); 
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y+height,	z);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x,		  y,		z);
+	glEnd();
+ 
+	glBindTexture(GL_TEXTURE_2D,skybox[SKY_LEFT]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x,		  y,		z+length);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x,		  y+height, z+length);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x+width, y+height, z+length); 
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x+width, y,		z+length);
+	glEnd();
+
+	glBindTexture(GL_TEXTURE_2D,skybox[SKY_DOWN]);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(x+width, y,		z+length); 
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(x+width, y,		z);
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(x,		  y,		z);
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(x,		  y,		z+length);
+	glEnd();
+ 	glDisable(GL_TEXTURE_2D);
+
+}
+
+
+
+/*void draw_ground()
+{		
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D,moon);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(5000,-10,5000);
+	glTexCoord2f(800.0f, 0.0f); glVertex3f(5000,-10,-5000);
+	glTexCoord2f(800.0f, 800.0f); glVertex3f(-5000,-10,-5000);
+	glTexCoord2f(0.0f, 800.0f); glVertex3f(-5000,-10,5000);
+	glEnd();
+	glDisable(GL_TEXTURE_2D);
+	glLineWidth(5.0);
+	glTranslatef(0.0, -2, 0.0);
+	glTranslatef(0.0, 2, 0.0);
+}*/
+
+void idle()
+{
+	display();
+
+}
+
+void display(){
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glColor4f(1.0,1.0,1.0,1.0);
+	glLoadIdentity();		
+	gluLookAt(viewer[0], viewer[1], viewer[2],camera[0], camera[1], camera[2],0, 1, 0);
+	glRotatef(x_r, 0, 1, 0);
+	glRotatef(y_r, 0, 0, 1);
+	glRotatef(z_r, 1, 0, 0);
+
+	Draw_Skybox(viewer[0]+(0.05*movcord[0]),viewer[1]+(0.05*movcord[1]),viewer[2]+(0.05*movcord[2]),250,250,250);
+	glTranslatef(movcord[0],movcord[1],movcord[2]);
+	//draw_ground();  
+	terrainGen();
+	glPushMatrix();
+	glTranslatef(80,0,165);
+	
+	
+	glutSwapBuffers();
+
+}
+
+void displayReshape(int width,int height)
+{
+	glViewport(0,0,width,height);						
+	glMatrixMode(GL_PROJECTION);						
+	glLoadIdentity();
+	gluPerspective(65,(GLfloat)width/(GLfloat)height,0.01f,1000.0f);
+	glMatrixMode(GL_MODELVIEW);						
+	glLoadIdentity();			
+}
+
+
+void windowSpecial(int key,int x,int y){
+	
+	if(key==GLUT_KEY_UP)
+	{
+		
+		y_r+=3;	
+	}
+	if(key== GLUT_KEY_DOWN)
+	{
+		y_r-=3;
+	}
+	if(key==GLUT_KEY_RIGHT) x_r+=3;
+	if(key==GLUT_KEY_LEFT) x_r-=3;
+
+
+	display();
+
+}
+
+
+void  kb(unsigned char key, int x, int y)
+{   
+	if(key=='+') movcord[1]--;
+	if(key=='-') if(movcord[1] <0) movcord[1]++;
+	if(key=='*')
+	{
+		movcord[0]+=5*cos(-1*x_r*3.14/180.0);
+		movcord[2]+=5*sin(1*x_r*3.14/180.0);
+	}
+	if(key=='/')
+	{
+		movcord[0]-=5*cos(-1*x_r*3.14/180.0);
+		movcord[2]-=5*sin(1*x_r*3.14/180.0);
+	}
+	if(key=='r')
+	{
+		x_r=0; y_r=0; z_r=0;
+		viewer[0] = 1.0; viewer[1] = 0.0; viewer[2] = 0.0;
+		camera[0] = 0.0; camera[1] = 0.0; camera[2] = 0.0;
+	}
+	
+	
+	display();
+}
+
+void handleMouse(int x,int y)
+{
+	if((x-prevx)>=0)
+	x_r+=1;
+	else
+	x_r-=1;
+	prevx=x;
+}
+
+void passiveMouse(int x, int y)
+{
+	prevx=x;
+}
+
+
+int main(int argc, char** argv)
+{
+		glutInit(&argc,argv);
+		glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
+		glutInitWindowSize(800,600);
+		glutCreateWindow("Lunar Surface!");
+		initLights();
+		initSky();
+  		glutDisplayFunc(display);
+	 	glutReshapeFunc(displayReshape);
+	 	glutKeyboardFunc(kb);
+		glutMotionFunc(handleMouse);
+		glutPassiveMotionFunc(passiveMouse);
+		glutIdleFunc(idle);
+		glutSpecialFunc(windowSpecial);
+		glutMainLoop();
+		return 0;
+}
+
